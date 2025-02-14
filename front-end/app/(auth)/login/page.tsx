82565a0d1734926034c73cb6a274107b8d959217
Apple <apple@Sethas-book.local>
2025-02-14 22:32:49 +0700

    login

--- front-end/app/(auth)/login/page.tsx
+++ front-end/app/(auth)/login/page.tsx
@@ -1,15 +1,57 @@
-import NavBar from "@/layouts/NavBar";
-import React from "react";
+"use client";
 
-const page = () => {
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { PasswordInput, TextInput } from "@/components/customeInput";
+
+
+export default function LoginPage() {
   return (
-    <div>
-      page
-      <div className="min-h-screen pb-16">
-        <NavBar />
+    <div className="min-h-screen flex flex-col items-center justify-center px-4">
+      <div className="w-full max-w-md space-y-8">
+        <div className="text-center">
+          <h1 className="header">JayLoy</h1>
+          <p className="description-medium mt-2">Spend wisely, waste less, save more</p>
+        </div>
+
+        <form className="space-y-4">
+          <TextInput type="text" placeholder="Username" />
+          <PasswordInput placeholder="Password" />
+
+          <div className="text-center">
+            <Link href="#" className="hover:underline description-small flex justify-end">Forget password</Link>
+          </div>
+
+          {/* <div className="text-center description-small">
+            By creating your account, you're agreeing to our{" "}
+            <Link href="#" className="underline hover:text-primary">Terms and Conditions</Link>{" "}
+            and{" "}
+            <Link href="#" className="underline hover:text-primary">Privacy Policy</Link>.
+          </div> */}
+
+          <Button className="green-button !text-white">Log In</Button>
+
+          <div className="relative description-small">
+            <div className="absolute inset-0 flex items-center">
+              <div className="w-full border-t"></div>
+            </div>
+            <div className="relative flex justify-center text-xs uppercase">
+              <span className="bg-background px-2 text-muted-foreground">or</span>
+            </div>
+          </div>
+
+          <Button variant="outline" className="w-full rounded-full">
+            <img src="https://www.google.com/favicon.ico" alt="Google" className="mr-2 h-4 w-4" />
+            Continue with Google
+          </Button>
+
+          <div className="text-center ">
+            <Link href="#" className="description-small hover:underline">
+              Skip
+            </Link>
+          </div>
+        </form>
       </div>
     </div>
   );
-};
-
-export default page;
+}
