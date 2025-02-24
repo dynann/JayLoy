1e21a94b75fa878508d990c7aacddad76ea2ecdd
SunLeang123 <leangkiyan2020@gmail.com>
2025-02-24 23:42:31 +0700

    refactor code

---
+++ front-end/app/constants/categories.tsx
@@ -0,0 +1,84 @@
+import { Icon } from "@iconify/react";
+import { JSX } from "react";
+
+export type TransactionCategory = {
+  id: number;
+  name: string;
+  icon: JSX.Element;
+  color: string;
+};
+
+export const TRANSACTION_CATEGORIES: { [key: number]: TransactionCategory } = {
+  1: {
+    id: 1,
+    name: "Food",
+    icon: <Icon icon="fluent:food-48-regular" width="3em" height="3em" className="text-white" />,
+    color: "bg-red",
+  },
+  2: {
+    id: 2,
+    name: "Transport",
+    icon: <Icon icon="solar:bus-bold" width="3em" height="3em" className="text-white" />,
+    color: "bg-purple",
+  },
+  3: {
+    id: 3,
+    name: "Medicine",
+    icon: <Icon icon="cuida:medicine-outline" width="3em" height="3em" className="text-white" />,
+    color: "bg-green",
+  },
+  4: {
+    id: 4,
+    name: "Groceries",
+    icon: <Icon icon="material-symbols:grocery" width="3em" className="text-white" />,
+    color: "bg-orange",
+  },
+  5: {
+    id: 5,
+    name: "Savings",
+    icon: <Icon icon="hugeicons:money-saving-jar" width="3em" className="text-white" />,
+    color: "bg-cyan",
+  },
+  6: {
+    id: 6,
+    name: "Rent",
+    icon: <Icon icon="mdi:house-clock-outline" width="3em" height="3em" className="text-white" />,
+    color: "bg-blue",
+  },
+  7: {
+    id: 7,
+    name: "Gifts",
+    icon: <Icon icon="famicons:gift-outline" width="3em" height="3em" className="text-white" />,
+    color: "bg-purple",
+  },
+  8: {
+    id: 8,
+    name: "Entertainment",
+    icon: <Icon icon="ion:ticket-outline" width="3em" height="3em" className="text-white" />,
+    color: "bg-pink",
+  },
+  9: {
+    id: 9,
+    name: "Internet",
+    icon: <Icon icon="material-symbols:wifi-rounded" width="3em" height="3em" className="text-white" />,
+    color: "bg-primary",
+  },
+  10: {
+    id: 10,
+    name: "Salary",
+    icon: <Icon icon="si:wallet-detailed-duotone" width="3em" height="3em" className="text-white" />,
+    color: "bg-brown",
+  },
+  11: {
+    id: 11,
+    name: "Freelance",
+    icon: <Icon icon="tdesign:money" width="3em" height="3em" className="text-white" />,
+    color: "bg-cyan",
+  },
+  12: {
+    id: 12,
+    name: "Invest",
+    icon: <Icon icon="streamline:investment-selection" width="3em" height="3em" className="text-white" />,
+    color: "bg-gray",
+  },
+};
