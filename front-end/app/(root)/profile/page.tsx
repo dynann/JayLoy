9b2812576d6b071284d7005ae8e0469548f662e5
Apple <apple@Sethas-book.local>
2025-03-07 21:25:17 +0700

    logg  out

--- front-end/app/(root)/profile/page.tsx
+++ front-end/app/(root)/profile/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 import React, { useEffect, useState } from "react";
+import { signOut, useSession } from "next-auth/react"; 
 import Image from "next/image";
+import SVG from "react-inlinesvg";
 import ProfileImage from "@/public/images/plant.webp";
 import { Icon } from "@iconify/react";
-import { useRouter } from "next/navigation";
-function page() {
-  const containerClasses =
-    "min-h-screen flex flex-col items-center justify-center px-4 gap-2";
+import router, { useRouter, useSearchParams } from "next/navigation";
+ function page() {
+  const containerClasses ="min-h-screen flex flex-col items-center justify-center px-4 gap-2";
   const router = useRouter();
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
@@ -15,21 +16,21 @@ function page() {
   const handleLogout = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
-      const res = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/auth/handleLogout`,
-        {
-          method: "PATCH",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${localStorage.getItem("refreshToken")}`, //remove refreshToken in db
-          },
+
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/logout`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("refreshToken")}`,  //remove refreshToken in db
         }
-      );
+      });
       if (res.ok) {
-        localStorage.removeItem("refreshToken"); //  remove refreshToken in  client side
-        console.log("handleLogout successfully");
-        router.push("/login");
-      } else {
+      localStorage.removeItem("accessToken");
+      localStorage.removeItem("tokenTimestamp"); 
+      localStorage.removeItem("refreshToken");//remove in client side
+         console.log("logout successfully")
+         router.push("/login");
+      }  else {
         setError("Failed to fetch user data");
       }
     } catch (err) {
@@ -37,24 +38,26 @@ function page() {
       console.error("Failed to fetch", err);
     }
   };
+
+ 
+   
+
+  // const handleLogout = async () => {  //not done yet, call api and use instead
+  //   await logout();
+  //   // Clear localStorage 
+  //   if (typeof window !== "undefined") {
+  //     localStorage.removeItem("accessToken");
+  //     localStorage.removeItem("refreshToken");
+  //     localStorage.removeItem("tokenTimestamp");
+  //   }
+  //   router.push("/login");
+  // };
   const lists = [
-    {
-      title: "Edit Profile",
-      icon: <Icon icon="iconamoon:profile-fill" width="24" height="24" />,
-    },
+    { title: "Edit Profile", icon: <Icon icon="iconamoon:profile-fill" width="24" height="24" />},
     { title: "My Data", icon: <Icon icon="bxs:data" width="24" height="24" /> },
-    {
-      title: "Setting",
-      icon: <Icon icon="lets-icons:setting-fill" width="24" height="24" />,
-    },
-    {
-      title: "Terms and Conditions",
-      icon: <Icon icon="famicons:document-text" width="24" height="24" />,
-    },
-    {
-      title: "Log out",
-      icon: <Icon icon="famicons:document-text" width="24" height="24" />,
-    },
+    { title: "Setting", icon: <Icon icon="lets-icons:setting-fill" width="24" height="24" /> },
+    { title: "Terms and Conditions", icon: <Icon icon="famicons:document-text" width="24" height="24" />},
+    { title: "Log out", icon: <Icon icon="famicons:document-text" width="24" height="24" /> },
   ];
   return (
     <div className={containerClasses}>
@@ -65,34 +68,23 @@ function page() {
           src={ProfileImage}
           alt="Neil image"
         />
-        <p className="description-regular text-center">
-          {username || "Loading..."}
-        </p>
-        <p className="description-regular text-center">
-          {email || "Loading..."}
-        </p>
+        <p className="description-regular text-center">{username || "Loading..."}</p>
+        <p className="description-regular text-center">{email || "Loading..."}</p>
 
         {/* the setting list  */}
         <div>
           {lists.map((item, index) => (
             <ul className=" ">
-              <li key={index} className="m-2 sm:pb-1 ">
-                <div
-                  className="flex  items-center w-full space-x-4 rtl:space-x-reverse"
-                  onClick={item.title === "Log out" ? handleLogout : undefined}
-                >
+              <li key={index} className="m-2 sm:pb-1 " >
+                <div className="flex  items-center w-full space-x-4 rtl:space-x-reverse"
+                 onClick= {item.title === "Log out"? handleLogout : undefined} >
                   {/* icon  */}
                   <div className="flex flex-col items-center justify-center py-2 text-gray">
-                    <div
-                      className={`bg-tertiary p-2 rounded-lg text-primary shadow-sm`}
-                    >
-                      {item.icon}
-                    </div>
+                     <div className={`bg-tertiary p-2 rounded-lg text-primary shadow-sm`}>{item.icon}</div>
                   </div>
                   <div className="flex-1  min-w-0">
-                    <p
-                      className=" description-regular text-left text-black truncate dark:text-white"
-                      // onClick= {item.title === "Log out"? handleLogout : undefined}
+                    <p className=" description-regular text-left text-black truncate dark:text-white"
+                    // onClick= {item.title === "Log out"? handleLogout : undefined}
                     >
                       {item.title}{" "}
                     </p>
